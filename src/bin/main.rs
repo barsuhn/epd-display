1bdc6986a3ef7e0fd1bf4f08ecd3b3f05d5893a6
Bernd Barsuhn <bernd.barsuhn@me.com>
2025-06-15 02:44:09 +0200

    Spawning tasks in both cores

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -5,6 +5,7 @@ use defmt::info;
 use embassy_executor::Executor;
 use embassy_rp::bind_interrupts;
 use embassy_rp::peripherals::SPI1;
+use embassy_rp::multicore::{spawn_core1, Stack};
 use embassy_time::Timer;
 use embedded_graphics::mono_font::MonoTextStyle;
 use embedded_graphics::mono_font::ascii::FONT_10X20;
@@ -19,7 +20,9 @@ use display::color::ThreeColor;
 use display::display_spi::DisplaySpi;
 use display::epd_2in66b::Epd2in66b;
 
+static mut CORE1_STACK: Stack<40960> = Stack::new();
 static EXECUTOR0: StaticCell<Executor> = StaticCell::new();
+static EXECUTOR1: StaticCell<Executor> = StaticCell::new();
 static DISPLAY: StaticCell<Epd2in66b<SPI1>> = StaticCell::new();
 
 bind_interrupts!(
@@ -89,7 +92,7 @@ fn draw(display: &mut Epd2in66b<SPI1>) {
 }
 
 #[embassy_executor::task]
-async fn run(display: &'static mut Epd2in66b<SPI1>) {
+async fn run_display(display: &'static mut Epd2in66b<SPI1>) {
     info!("initializing display");
     display.init().await;
 
@@ -103,24 +106,47 @@ async fn run(display: &'static mut Epd2in66b<SPI1>) {
     display.sleep().await;
 
     loop {
-        info!("tick");
-        Timer::after_secs(1).await;
+        info!("tick display");
+        Timer::after_secs(5).await;
     }
 }
+
+#[embassy_executor::task]
+async fn run_wifi() {
+    loop {
+        info!("tick wifi");
+        Timer::after_secs(5).await;
+    }
+}
+
+
 
 #[cortex_m_rt::entry]
 fn main() -> ! {
     let p = embassy_rp::init(Default::default());
-    let executor0 = EXECUTOR0.init(Executor::new());
-    let spi_ifc = DisplaySpi::new(p.SPI1, p.PIN_9, p.PIN_10, p.PIN_11, p.DMA_CH0);
-    let display = DISPLAY.init(Epd2in66b::new(spi_ifc, p.PIN_13, p.PIN_8, p.PIN_12));
 
-    executor0.run(|spawner| {
+    spawn_core1(p.CORE1, unsafe { &mut *core::ptr::addr_of_mut!(CORE1_STACK)  }, move || {
         info!("display task spawning");
 
-        match spawner.spawn(run(display)) {
-            Ok(_) => info!("display task started"),
-            Err(_) => info!("display task failed"),
+        let spi_ifc = DisplaySpi::new(p.SPI1, p.PIN_9, p.PIN_10, p.PIN_11, p.DMA_CH0);
+        let display = DISPLAY.init(Epd2in66b::new(spi_ifc, p.PIN_13, p.PIN_8, p.PIN_12));
+
+        let executor1 = EXECUTOR1.init(Executor::new());
+        executor1.run(|spawner|
+            match spawner.spawn(run_display(display)) {
+                Ok(_) => info!("core 1 display task started"),
+                Err(e) => info!("core 1 display task failed: {:?}", e),
+            }
+        );
+    });
+
+    let executor0 = EXECUTOR0.init(Executor::new());
+    executor0.run(|spawner| {
+        info!("wifi task spawning");
+
+        match spawner.spawn(run_wifi()) {
+            Ok(_) => info!("core 0 wifi task started"),
+            Err(_) => info!("core 0 wifi task failed"),
         }
     });
 }
