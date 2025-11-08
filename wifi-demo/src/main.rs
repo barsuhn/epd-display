22fc2eb8dc39c702422e0fc97b318771e34adfe9
Bernd Barsuhn <bernd.barsuhn@me.com>
2025-11-08 22:47:15 +0100

    Refactored WIFI setup into three tasks to minimize memory footprint

--- wifi-demo/src/main.rs
+++ wifi-demo/src/main.rs
@@ -16,14 +16,13 @@ use embassy_rp::clocks::RoscRng;
 use embassy_rp::gpio::{Level, Output};
 use embassy_rp::peripherals::{DMA_CH0, PIN_23, PIN_24, PIN_25, PIN_29, PIO0};
 use embassy_rp::pio::{InterruptHandler, Pio};
-//use embassy_time::Timer;
+use embassy_sync::channel::{Channel, Sender, Receiver};
+use embassy_sync::blocking_mutex::raw::NoopRawMutex;
 use embassy_time::Duration;
 use embedded_io_async::Write;
 use static_cell::StaticCell;
 
 use dev_tools::stack_paint::{measure_stack_usage, paint_stack};
-
-static EXECUTOR0: StaticCell<Executor> = StaticCell::new();
 
 bind_interrupts!(
     struct Irqs {
@@ -48,12 +47,24 @@ struct WifiHardware {
     net_device: cyw43::NetDriver<'static>,
 }
 
+struct WifiResources {
+    stack: embassy_net::Stack<'static>,
+    control: cyw43::Control<'static>,
+}
+
+// This allocates memory for 3 sockets. DHCP and DNS each require one socket.
+static STACK_RESOURCES: StaticCell<StackResources<3>> = StaticCell::new();
+static CYW43_STATE: StaticCell<cyw43::State> = StaticCell::new();
+static INIT_CHANNEL: StaticCell<Channel<NoopRawMutex, WifiHardware, 1>> = StaticCell::new();
+static CONNECT_CHANNEL: StaticCell<Channel<NoopRawMutex, WifiResources, 1>> = StaticCell::new();
+static EXECUTOR0: StaticCell<Executor> = StaticCell::new();
+
 #[cortex_m_rt::entry]
 fn main() -> ! {
-    unsafe { paint_stack(); }
-
+    let init_channel = INIT_CHANNEL.init(Channel::new());
+    let connect_channel = CONNECT_CHANNEL.init(Channel::new());
+    let executor0 = EXECUTOR0.init(Executor::new());
     let p = embassy_rp::init(Default::default());
-
     let wifi_peripherals = WifiPeripherals {
         pwr_pin: p.PIN_23,
         cs_pin: p.PIN_25,
@@ -63,25 +74,61 @@ fn main() -> ! {
         dma: p.DMA_CH0,
     };
 
-    let executor0 = EXECUTOR0.init(Executor::new());
     executor0.run(|spawner| {
         info!("wifi task spawning");
 
-        match spawner.spawn(run_wifi(spawner, wifi_peripherals)) {
-            Ok(_) => info!("wifi task started"),
-            Err(_) => info!("wifi task failed"),
+        match spawner.spawn(run_wifi_init(spawner, wifi_peripherals, init_channel.sender())) {
+            Ok(_) => info!("wifi init task started"),
+            Err(_) => info!("wifi init task failed"),
+        }
+
+        match spawner.spawn(run_wifi_connect(spawner, init_channel.receiver(), connect_channel.sender())) {
+            Ok(_) => info!("connect task started"),
+            Err(_) => info!("connect task failed"),
+        }
+
+        match spawner.spawn(run_server(connect_channel.receiver())) {
+            Ok(_) => info!("server task started"),
+            Err(_) => info!("server task failed")
         }
     });
 }
 
 #[embassy_executor::task]
-async fn run_wifi(spawner: Spawner, peripherals: WifiPeripherals) {
-    let wifi_hardware = init_wifi_hardware(&spawner, peripherals).await;
-    let mut control = wifi_hardware.control;
-    let net_device = wifi_hardware.net_device;
-    let stack = connect_to_network(&spawner, &mut control, net_device).await;
+async fn run_wifi_init(spawner: Spawner, peripherals: WifiPeripherals, sender: Sender<'static, NoopRawMutex, WifiHardware, 1>) {
+    unsafe { paint_stack(); }
+
+    let mut wifi_hardware = init_wifi_hardware(&spawner, peripherals).await;
 
     unsafe { measure_stack_usage(); }
+
+    let clm = include_bytes!("../../cyw43-firmware/43439A0_clm.bin");
+
+    wifi_hardware.control.init(clm).await;
+    wifi_hardware.control
+        .set_power_management(cyw43::PowerManagementMode::PowerSave)
+        .await;
+
+    sender.send(wifi_hardware).await;
+}
+
+#[embassy_executor::task]
+async fn run_wifi_connect(spawner: Spawner, receiver: Receiver<'static, NoopRawMutex, WifiHardware, 1>, sender: Sender<'static, NoopRawMutex, WifiResources, 1>) {
+    let WifiHardware{ mut control, net_device} = receiver.receive().await;
+
+    unsafe { paint_stack(); }
+
+    let stack = connect_to_network(&spawner, &mut control, net_device).await;
+    let resources = WifiResources{ stack, control };
+
+    unsafe { measure_stack_usage(); }
+
+    sender.send(resources).await;
+}
+
+#[embassy_executor::task]
+async fn run_server(receiver: Receiver<'static, NoopRawMutex, WifiResources, 1>) {
+    let WifiResources{ stack, mut control} = receiver.receive().await;
 
     run_tcp_server(stack, &mut control).await
 }
@@ -91,7 +138,6 @@ async fn init_wifi_hardware(
     peripherals: WifiPeripherals,
 ) -> WifiHardware {
     let fw = include_bytes!("../../cyw43-firmware/43439A0.bin");
-    let clm = include_bytes!("../../cyw43-firmware/43439A0_clm.bin");
 
     let pwr = Output::new(peripherals.pwr_pin, Level::Low);
     let cs = Output::new(peripherals.cs_pin, Level::High);
@@ -107,15 +153,13 @@ async fn init_wifi_hardware(
         peripherals.dma,
     );
 
-    static STATE: StaticCell<cyw43::State> = StaticCell::new();
-    let state = STATE.init(cyw43::State::new());
-    let (net_device, mut control, runner) = cyw43::new(state, pwr, spi, fw).await;
-    unwrap!(spawner.spawn(cyw43_task(runner)));
+    let state = CYW43_STATE.init(cyw43::State::new());
+    let (net_device, control, runner) =cyw43::new(state, pwr, spi, fw).await;
 
-    control.init(clm).await;
-    control
-        .set_power_management(cyw43::PowerManagementMode::PowerSave)
-        .await;
+    match spawner.spawn(cyw43_task(runner)) {
+        Ok(_) => info!("Cyw43 runner task spawned"),
+        Err(_) => info!("Cyw43 runner task failed")
+    };
 
     WifiHardware {
         control,
@@ -128,15 +172,15 @@ async fn connect_to_network(
     control: &mut cyw43::Control<'static>,
     net_device: cyw43::NetDriver<'static>,
 ) -> embassy_net::Stack<'static> {
-    let config = Config::dhcpv4(Default::default());
-
     let mut rng = RoscRng;
     let seed = rng.next_u64();
+    let config = Config::dhcpv4(Default::default());
+    let (stack, runner) = embassy_net::new(net_device, config, STACK_RESOURCES.init(StackResources::new()), seed);
 
-    static RESOURCES: StaticCell<StackResources<3>> = StaticCell::new();
-    let (stack, runner) = embassy_net::new(net_device, config, RESOURCES.init(StackResources::new()), seed);
-
-    unwrap!(spawner.spawn(net_task(runner)));
+    match spawner.spawn(net_task(runner)) {
+        Ok(_) => info!("Net runner task spawned"),
+        Err(_) => info!("Net runner task failed")
+    }
 
     while let Err(err) = control
         .join(WIFI_NETWORK, JoinOptions::new(WIFI_PASSWORD.as_bytes()))
@@ -186,8 +230,6 @@ async fn run_tcp_server(
         info!("Received connection from {:?}", socket.remote_endpoint());
         control.gpio_set(0, true).await;
 
-        unsafe { measure_stack_usage(); }
-
         loop {
             let n = match socket.read(&mut buf).await {
                 Ok(0) => {
@@ -211,8 +253,6 @@ async fn run_tcp_server(
                 }
             };
         }
-
-        unsafe { measure_stack_usage(); }
     }
 }
 
